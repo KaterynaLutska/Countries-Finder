13a9c05f773e9f4ad1084538604bbf424f4e5c03
Kateryna Lutska <ket.lutska@gmail.com>
2021-01-04 16:50:28 +0200

    add delay

--- src/js/components/notifications.js
+++ src/js/components/notifications.js
@@ -1,14 +1,30 @@
-import { error } from "@pnotify/core/dist/PNotify.js";
+import {
+  notice,
+  error,
+  defaultModules,
+  defaults,
+} from "@pnotify/core/dist/PNotify.js";
 import "@pnotify/mobile/dist/PNotifyMobile.js";
 import "@pnotify/core/dist/BrightTheme.css";
 import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/mobile/dist/PNotifyMobile.css";
+import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile.js";
+
+// function errorNotify() {
+//   error({
+//     title: "Йой!",
+//     text:
+//
+//     hide: false,
+//   });
+//   defaultModules.set(PNotifyMobile, {});
+// }
 
 function errorNotify() {
-  error({
-    title: "Йой!",
-    text:
-      "Кількість країн перевищує можливості нашого сервера. Оберіть конкретну країну!",
-    hide: false,
+  notice({
+    title: "ЙоЙ!",
+    text: "Таку кількість країн нам не облетіти! Оберіть одну країну!",
+    delay: 3000,
   });
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import refs from "./refs";
 import countryCardMarcup from "../templates/country.hbs";
 import errorNotify from "./components/notifications";
-import debounce from "lodash.debounce";
+//import debounce from "lodash.debounce";
 import API from "./api-service.js";
+import countryName from "../templates/name-country.hbs";
 
 // функція відмальовки карточки країни //
 
@@ -11,34 +12,41 @@ refs.searchForm.addEventListener("input", findCountry);
 function renderCountryCard(country) {
   const marcup = countryCardMarcup(country);
   refs.countryCard.innerHTML = marcup;
-  console.dir(marcup);
+}
+
+function tenCountries(country) {
+  const marcup = countryName(country);
+  refs.countryCard.innerHTML = marcup;
 }
 
 // функція пошуку країни //
 function findCountry(event) {
   event.preventDefault();
-  //   const input = refs.input.value;
-  //   console.log(input);
 
   const input = event.currentTarget;
-
   const searchQuery = input.elements.query.value;
 
   API.fetchCountryByName(searchQuery)
-    .then(renderCountryCard) // якщо все добре, то на проміс ми вішаємо наступний then з необіхдними даними
+    .then((renderCountryCard) => {
+      renderCountryCard;
+      couuntryNumber(renderCountryCard);
+      console.log(renderCountryCard.length);
+    }) // якщо все добре, то на проміс ми вішаємо наступний then з необіхдними даними
     .catch((error) => {
+      // якщо все погано  - спіймали відповідь в catch
       console.log(error);
     });
-  // якщо все погано  - спіймали відповідь в catch
   //.finally(() => input.reset()); // очищуємо поля input
 }
 
-// function couuntryNumber(number) {
-//   if (number.length >= 2 && number.length <= 10) {
-//     return "10 країн";
-//   } else if (number.length > 10) {
-//     return errorNotify();
-//   } else if (number.length === 1) {
-//     return refs.searchForm.addEventListener("input", findCountry);
-//   }
-// }
+function couuntryNumber(countryNumber) {
+  if (countryNumber.length > 10) {
+    errorNotify();
+  } else if (countryNumber.length > 1 && countryNumber.length <= 10) {
+    //видалити нотіфікашку
+    tenCountries(countryNumber);
+  } else if (countryNumber.length === 1) {
+    //видалити нотіфікашку
+    renderCountryCard(countryNumber);
+  }
+}
